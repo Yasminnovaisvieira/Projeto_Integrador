9642439723e6e50b525f3e1eae2b352a2fedaaf8
Yasminnovaisvieira <yasminnovaisvieira789@gmail.com>
2025-06-08 16:32:03 -0300

    Projeto Integrador - 08/06/2025

--- server.py
+++ server.py
@@ -6,21 +6,6 @@ import ssl
 PORT = 8080  # Porta em que o servidor vai rodar
 DIRECTORY = "."
 
-
-# Está bem completo, ficou muito bom o site e o server
-# Acho q a unica coisa que eu faria de diferente é usar container para a aplicação em sí
-# E criar mais arquivos CSS para não ficar no código
-# Usar modularização do CSS seria uma boa
-
-# Acho que se estive usando uma estrutura mais robusta como React, Next, Vue ou Angular
-# você teria muito mais liberdade e seria um projeto muito mais profissional, pq o design
-# e codigo estão bem profissionais
-
-# Tutorial para limpar suas credenciais do GitHub
-# Gerenciador de Credenciais > Credenciais do Windows > git:https://github.com > Remover
-
-# Não esqueça de usar guia anônima :)
-
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
